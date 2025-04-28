68aa5984aec9dd426281b4b1709f02bd4ed543e2
ZagZx <zagzcraft@gmail.com>
2025-04-28 13:07:32 -0300

    background feito

--- change_back_color/app.py
+++ change_back_color/app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/', methods = ["POST"])
-def manda_beck():
-    cor = request.form['cor']
-    return cor
-
-@app.route('/background')
+@app.route('/background', methods = ['GET', 'POST'])
 def background():
-    return render_template('background.html')
+    if request.method == 'POST':
+        cor = request.form['cor']
+        return render_template('background.html', cor = cor)
+
+    return render_template('background.html', cor = None)
 
 if __name__ == '__main__':
     app.run(debug=True)

e487fae3ad83662a6b37d994d3c935cf2175ac85
ZagZx <azevedodvictor@gmail.com>
2025-04-28 08:14:57 -0300

    sla

--- change_back_color/app.py
+++ change_back_color/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
-    
+    return render_template('index.html')
 
+@app.route('/', methods = ["POST"])
+def manda_beck():
+    cor = request.form['cor']
+    return cor
 
+@app.route('/background')
+def background():
+    return render_template('background.html')
 
-
-
-
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)

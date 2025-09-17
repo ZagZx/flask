3235da057f962f76595db1d9dfb2cd7e7565b598
ZagZx <azevedodvictor@gmail.com>
2025-09-17 00:23:41 -0300

    mudança na estrutura das pastas && começo do projeto da loja

---
+++ projetos/loja/app/models/product.py
@@ -0,0 +1,21 @@
+from . import Base
+from app import db
+
+from sqlalchemy import (
+    String,
+    Integer,
+    DECIMAL,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id:Mapped[int] = mapped_column(Integer, primary_key=True)
+    name:Mapped[str] = mapped_column(String(255), nullable=False)
+    price:Mapped[float] = mapped_column(DECIMAL(10,2), nullable=False)
+    description:Mapped[str] = mapped_column(String, nullable=False)
---
+++ projetos/loja/app/models/user.py
@@ -0,0 +1,20 @@
+from . import Base
+from flask_login import UserMixin
+from app import db
+
+from sqlalchemy import (
+    String,
+    Integer,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id:Mapped[int] = mapped_column(Integer, primary_key=True)
+    name:Mapped[str] = mapped_column(String(255), nullable=False)
+    email:Mapped[str] = mapped_column(String(255), nullable=False)
+    password_hash:Mapped[str] = mapped_column(String(255), nullable=False)

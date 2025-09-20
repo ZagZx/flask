bc343bd93f6f2626b1d1d730a5535196d492c77d
ZagZx <azevedodvictor@gmail.com>
2025-09-20 02:13:55 -0300

    text na descrição

--- projetos/loja/app/models/product.py
+++ projetos/loja/app/models/product.py
@@ -4,6 +4,7 @@ from sqlalchemy import (
     String,
     Integer,
     DECIMAL,
+    TEXT
 )
 from sqlalchemy.orm import (
     Mapped,
@@ -17,4 +18,4 @@ class Product(db.Model):
     id:Mapped[int] = mapped_column(Integer, primary_key=True)
     name:Mapped[str] = mapped_column(String(255), nullable=False)
     price:Mapped[float] = mapped_column(DECIMAL(10,2), nullable=False)
-    description:Mapped[str] = mapped_column(String, nullable=False)
+    description:Mapped[str] = mapped_column(TEXT, nullable=False)

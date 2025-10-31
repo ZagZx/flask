a7829c44730cad650f820348ef2b65b341722f61
ZagZx <azevedodvictor@gmail.com>
2025-10-31 10:55:27 -0300

    relacionamento entre vendedor e produto

--- projetos/mvc/mvc/models/product.py
+++ projetos/mvc/mvc/models/product.py
@@ -1,6 +1,8 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Numeric, ForeignKey
+
 from mvc.extensions import Base
+from .user import User
 
 class Product(Base):
     __tablename__ = "product"
@@ -9,3 +11,6 @@ class Product(Base):
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str] = mapped_column(String(255), nullable=False)
     price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+    seller_id = Mapped[int] = mapped_column(ForeignKey(User.id))
+
+    seller = Mapped[User] = mapped_column(relationship(back_populates='sold_products'))
--- projetos/mvc/mvc/models/user.py
+++ projetos/mvc/mvc/models/user.py
@@ -1,8 +1,11 @@
 from flask_login import UserMixin
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
+from typing import List
 from werkzeug.security import check_password_hash
+
 from mvc.extensions import Base
+from .product import Product
 
 class User(UserMixin, Base):
     __tablename__ = "user"
@@ -11,6 +14,8 @@ class User(UserMixin, Base):
     username: Mapped[str] = mapped_column(String(30), nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    sold_products = Mapped[List[Product]] = relationship(back_populates='seller')
 
     def check_password(self, password: str):
         return check_password_hash(self.password_hash, password)
